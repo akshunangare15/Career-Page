c1f2678cfa4086ce0ed8c9b654e9b6e8f4d1eb6f
Bhavana Nangare <60853406+akshunangare15@users.noreply.github.com>
2022-09-17 13:37:16 -0700

    Add files via upload

--- src/components/mission/Mission.jsx
+++ src/components/mission/Mission.jsx
@@ -1,1 +1,21 @@
+import { useState } from "react";
+import "./mission.scss";
 
+export default function Mission() {
+
+
+  return (
+    <div className="mission" id="mission"> 
+     <div className = "right">
+      <h2>Our Mission</h2> 
+      
+  <h3>
+      Our platform helps customers unlock their creativity and make exceptional content while providing continuous support and educational resources 
+      based on their needs.
+    </h3>
+        
+      </div>
+      </div>
+  );
+
+}
